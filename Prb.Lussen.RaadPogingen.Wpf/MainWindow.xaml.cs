2d0dbdae7adaa6da07f48a17a20f768259c78b1f
dmouriss <dieter.mourisse@howest.be>
2020-10-11 15:41:05 +0200

    Update to .NET Core

--- Prb.Lussen.RaadPogingen.Wpf/MainWindow.xaml.cs
+++ Prb.Lussen.RaadPogingen.Wpf/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RaadPogingen.Wpf
+namespace Prb.Lussen.RaadPogingen.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
